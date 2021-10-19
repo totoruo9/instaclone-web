e08ea8a2ff0cdc3b473b38c90e1da5d4d64d3ad2
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-10-19 09:26:32 +0900

    [FIX TS1208 bug]

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -29,7 +29,7 @@ export const disableDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: process.env.NODE_ENV === "production" ? "https://instaclone-backend-jin.herokuapp.com/graphql" : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 import { logUserOut } from "../apollo";
 import routes from "../routes";
 
+const FEED_QUERY = gql`
+    query seeFeed {
+        seeFeed {
+            id
+            user {
+                username
+                avatar
+            }
+            file
+            caption
+            likes
+            comments
+            createAt
+            isMine
+        }
+    }
+`;
+
 const Home = () => {
     const history = useHistory();
+    history.replace(routes.home, null);
 
-    history.replace(routes.home, null)
+    const {data} = useQuery(FEED_QUERY);
+    console.log(data);
 
     return (
         <>
